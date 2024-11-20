8427eeeda4711621c84334973665a53ce4253307
dipto bkashi~ <diptibakshi72@gmail.com>
2024-11-20 18:26:18 +0600

    add

---
+++ fast/src/firebase/Routs/Routs.jsx
@@ -0,0 +1,16 @@
+import {
+    createBrowserRouter,
+  
+  } from "react-router-dom";
+import Mani from "../../Mani";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Mani></Mani>,
+      children: [
+       
+  
+  
+      ]
+    } ])
